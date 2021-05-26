850cd725089bb3b4a4aaa8a6be03bc50e49d0da2
Monika <m.szymanska.macik@gmail.com>
2021-05-26 15:50:33 +0200

    CQRS

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,8 @@ namespace OrderingAPizza
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IQueryExecutor, QueryExecutor>();
+
             services.AddAutoMapper(typeof(PizzasProfile).Assembly);
 
             services.AddMediatR(typeof(ResponseBase<>));
